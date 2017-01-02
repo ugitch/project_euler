d1ebf381eed74c7ac42a26bc51a12d48e2ee6a9e
Ugitch <ugison@gmail.com>
2017-01-03 00:14:22 +0100

    Best solution

--- js/problem_001.js
+++ js/problem_001.js
@@ -1,11 +1,13 @@
-// improved solution, less iterations
+/* best solution, based on previous one
+   for n = 3, 3+6+9+12+......+999=3*(1+2+3+4+...+333)
+   for n = 5, 5+10+15+...+995=5*(1+2+....+199)
+   199 is 995/5, but also 999/5 rounded down to nearest integer
+   Using mathematical truth 1+2+3+...+p=½*p*(p+1) and Math.floor
+*/
 function sumDivisibleBy(n, target) {
-  var sum = 0;
-  for (var i=0; i <=target; i+=n) {
-    if (i % n === 0)
-      sum += i;
-  }
-  return sum;
+  var p = Math.floor(target/n);
+
+  return Math.floor(n * p * (p + 1) / 2);
 }
 
 function sumMultiplesOfThreeAndFive() {
