1fc70f56f29ecba2159e72cfe303420a50a8a535
Ugitch <ugison@gmail.com>
2017-01-03 00:11:20 +0100

    Better solution

--- js/problem_001.js
+++ js/problem_001.js
@@ -1,10 +1,18 @@
-function sumMultiplesOfThreeAndFive(){
-  var input = document.getElementById("problem_input");
-  var answer = document.getElementById("answer");
+// improved solution, less iterations
+function sumDivisibleBy(n, target) {
   var sum = 0;
-  for (var i=1; i<input.value; i++){
-    if (i % 3 === 0 || i % 5 === 0)
+  for (var i=0; i <=target; i+=n) {
+    if (i % n === 0)
       sum += i;
   }
-  answer.innerHTML = sum;
+  return sum;
+}
+
+function sumMultiplesOfThreeAndFive() {
+  var input = document.getElementById("problem_input");
+  var answer = document.getElementById("answer");
+
+  var target = input.value - 1;
+
+  answer.innerHTML = sumDivisibleBy(3, target) + sumDivisibleBy(5, target) - sumDivisibleBy(15, target);
 }
