e1e84918e1f8b68460c95a7403ddc271768291fd
Ugitch <ugison@gmail.com>
2017-01-03 16:10:51 +0100

    Problem 2

--- js/problem_001.js
+++ js/problem_001.js
@@ -1,8 +1,10 @@
-/* best solution, based on previous one
-   for n = 3, 3+6+9+12+......+999=3*(1+2+3+4+...+333)
-   for n = 5, 5+10+15+...+995=5*(1+2+....+199)
-   199 is 995/5, but also 999/5 rounded down to nearest integer
-   Using mathematical truth 1+2+3+...+p=½*p*(p+1) and Math.floor
+/*  If we list all the natural numbers below 10 that are multiples   of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000. 
+  
+Best solution, based on previous one
+for n = 3, 3+6+9+12+......+999=3*(1+2+3+4+...+333)
+for n = 5, 5+10+15+...+995=5*(1+2+....+199)
+199 is 995/5, but also 999/5 rounded down to nearest integer
+Using mathematical truth 1+2+3+...+p=½*p*(p+1) and Math.floor
 */
 function sumDivisibleBy(n, target) {
   var p = Math.floor(target/n);
